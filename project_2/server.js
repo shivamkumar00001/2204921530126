9892315525a771a0e957d520b15ba6c374055d05
shivamkumar00001 <shivamkumar933401@gmail.com>
2025-05-08 12:58:40 +0530

    changes in server

--- project_2/server.js
+++ project_2/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('./node_modules/axios/index.d.cts');
+const axios = require('axios'); // Correct import
 const cors = require('cors');
 
 const app = express();
